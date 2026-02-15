a6759a34106bff608b8cdc2752649950ceedd0ef
mohamed231141758 <ophamed223@gmail.com>
2026-02-15 15:48:37 -0800

    Fix multiply and divide test cases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -24,12 +24,17 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
+        self.assertEqual(self.calc.multiply(1, 5), 5)
+        self.assertEqual(self.calc.multiply(5, 0), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(0, 5), 0)
         self.assertEqual(self.calc.divide(-10, 2), -5)
         self.assertIsNone(self.calc.divide(10, 0))
+        self.assertIsNone(self.calc.divide(0, 0))
+        self.assertEqual(self.calc.divide(9, 3), 3)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
 
 if __name__ == "__main__":
     unittest.main()
